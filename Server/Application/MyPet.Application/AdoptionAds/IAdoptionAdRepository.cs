10fd6a577b3d319236c212376115af9e43f0d54c
Kristian Lyubenov <kristian.lubenov@gmail.com>
2020-09-21 18:21:42 +0300

    Added repository contracts

---
+++ Server/Application/MyPet.Application/AdoptionAds/IAdoptionAdRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPet.Application.AdoptionAds
+{
+    using MyPet.Application.Common.Contracts;
+    using MyPet.Domain.AdoptionAds.Models;
+
+    public interface IAdoptionAdRepository : IRepository<AdoptionAd>
+    {
+        
+    }
+}
---
+++ Server/Application/MyPet.Application/MedicalRecords/IMedicalRecordRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPet.Application.MedicalRecords
+{
+    using MyPet.Application.Common.Contracts;
+    using MyPet.Domain.MedicalRecords.Models;
+
+    public interface IMedicalRecordRepository : IRepository<MedicalRecord>
+    {
+        
+    }
+}
